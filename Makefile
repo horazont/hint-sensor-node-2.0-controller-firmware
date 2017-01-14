c1b588b9121b6df75ebbaf86210bc0729acc62c5
Jonas Wielicki <j.wielicki@sotecware.net>
2017-01-14 12:50:49 +0100

    Working period counter

--- Makefile
+++ Makefile
@@ -3,25 +3,44 @@ LD      = arm-none-eabi-ld -v
 CP      = arm-none-eabi-objcopy
 OD      = arm-none-eabi-objdump
 
-CFLAGS  = -std=c11 -Wall -Wextra -Werror -I./ -c -fno-common -O0 -g -mcpu=cortex-m3 -mthumb
-LFLAGS  = -Tstm32.ld -nostartfiles
+PLATFORM = STM32F10X_MD
+STM_STDPERIPH_DEBUG = 1
+
+CFLAGS  = -std=c11 -Wall -Wextra -Werror -I./ -fno-common -O0 -ggdb -mcpu=cortex-m3 -mthumb -nostartfiles
+LFLAGS  = -Tstm32_flash.ld  -L/usr/lib/arm-none-eabi/newlib/libc.a -nostartfiles --gc-sections
 CPFLAGS = -Obinary
 ODFLAGS = -S
 
+c_sources = $(wildcard *.c)
+objs = $(patsubst %.c,%.o,$(c_sources))
+
+startup_file_src = startup_stm32f10x_md.s
+startup_file_obj = $(patsubst %.s,%.o,$(startup_file_src))
+
 all: test
 
-clean:
-	-rm -f main.lst main.o main.elf main.lst main.bin
+include ../stm32_spl/ST_STM32StdPeriph.mk
 
-test: blinky.elf
+system_obj = $($(PFX)_PFP)obj/CMSIS/Device/ST/STM32F10x/Source/system_stm32f10x.o
+
+clean: vendor_st_stm32_stdperiph_clean
+	-rm -f main.lst main.elf main.lst main.bin $(objs) $(startup_file_obj)
+
+test: main.elf
 	@ echo "...copying"
 	$(CP) $(CPFLAGS) main.elf main.bin
 	$(OD) $(ODFLAGS) main.elf > main.lst
 
-blinky.elf: main.o stm32.ld
-	@ echo "..linking"
-	$(LD) $(LFLAGS) -o main.elf main.o
 
-main.o: main.c
-	@ echo ".compiling"
-	$(CC) $(CFLAGS) main.c
+main.elf: $(objs) $(startup_file_obj) stm32_flash.ld $(SPL_LIB) $(system_obj)
+	@ echo "..linking"
+	$(LD) $(LFLAGS) -o main.elf $(startup_file_obj) $(objs) $(SPL_LIB) $(system_obj)
+
+# main.elf: $(objs) $(startup_file_obj) stm32_flash.ld $(SPL_LIB) $(system_obj)
+# 	$(CC) $(CFLAGS) -Wl,-v -Wl,-Tstm32_flash.ld -L/usr/lib/arm-none-eabi/newlib/libc.a -Wl,-nostartfiles -Wl,--gc-sections -o main.elf $(startup_file_obj) $(objs) $(SPL_LIB) $(system_obj)
+
+$(objs): %.o: %.c
+	$(CC) $(CFLAGS) -c -o $@ $<
+
+$(startup_file_obj): %.o: %.s
+	$(CC) $(CFLAGS) -c -o $@ $<
--- main.c
+++ main.c
@@ -1,40 +1,37 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
 
 #include "stm32f10x.h"
 
 #define STACK_TOP (void*)(0x20002000)
 
-void nmi_handler();
-void hardfault_handler();
 int main();
 
-
-void *myvectors[] __attribute__((section("vectors"))) = {
-    STACK_TOP,
-    main,
-    nmi_handler,
-    hardfault_handler,
-    hardfault_handler,
-    hardfault_handler,
-    hardfault_handler,
-};
-
-
-void nmi_handler() {
-    GPIOC->BSRR = 1<<8;
-    return;
+char nybble_to_hex(const uint8_t nybble)
+{
+    const uint8_t safe_nybble = nybble & 0xF;
+    if (safe_nybble >= 0xa) {
+        return 87 + safe_nybble;
+    } else {
+        return 48 + safe_nybble;
+    }
 }
 
-void hardfault_handler() {
-    GPIOC->BSRR = 1<<9;
-    return;
+void uint32_to_hex(const uint32_t value, char *buf)
+{
+    uint32_t shift = 28;
+    for (uint_fast8_t i = 0; i < 8; ++i) {
+        const uint8_t nybble = (value >> shift) & 0xF;
+        buf[i] = nybble_to_hex(nybble);
+        shift -= 4;
+    }
 }
 
 
 void delay() {
-    uint32_t i = 10000;
-    while (i-- > 0) {
+    for (uint32_t i = 0; i < 800000; ++i) {
         __asm__ volatile("nop");
     }
 }
@@ -43,109 +40,117 @@ static const char hello_world[] = "Hello World!";
 
 
 int main() {
-    RCC->AHBENR |=
-        (1 << 1)  // enable DMA1
-        | (1 << 0)  // enable DMA2
-        ;
-    RCC->APB2ENR |=
-        (1 << 14) // enable USART1
-        | (1 << 9)  // enable ADC1
-        | (1 << 5) // enable IO port D
-        | (1 << 4) // enable IO port C
-        | (1 << 2) // enable IO port A
-        | (1 << 0) // enable alternate function I/O
-        ;
-    /* RCC->APB2RSTR |= (1 << 14); */
-    GPIOC->CRH = 0x11; // enable two IOs for LEDs
-    GPIOC->CRL = 0;
-    GPIOA->CRL = 0x04;
+    RCC->APB1RSTR |= RCC_APB1RSTR_TIM3RST | RCC_APB1RSTR_TIM2RST;
+    RCC->APB2RSTR |= RCC_APB2RSTR_IOPARST | RCC_APB2RSTR_IOPBRST | RCC_APB2RSTR_IOPCRST | RCC_APB2RSTR_IOPDRST | RCC_APB2RSTR_IOPERST;
 
-    ADC1->CR2 =
-        (1 << 0) // wake up ADC
-        ;
-
-    GPIOA->CRH =
-        (0b11 << 4) | (0b10 << 6) // setup USART1 TX output
-        ;
-    GPIOC->BSRR = 1<<24;
-    GPIOC->BSRR = 1<<25;
-
-    USART1->BRR = 0x340;  // baud rate divider to 9600 baud at 8 MHz
-    USART1->CR1 = (1 << 13) | (1 << 3); // enable USART, enable TX
-    USART1->CR2 = 0;
-    USART1->CR3 = (1 << 7); // enable DMA
-
-    ADC1->CR1 = (1 << 8); // scan mode
-    ADC1->CR2 =
-        (1 << 8) // enable DMA
-        | (1 << 1) // continuous mode
-        ;
-    ADC1->SQR1 = (12 << 0) | (13 << 5) | (16 << 10) | (17 << 15) | (0b1111 << 20);
-    ADC1->SQR2 = (6 << 0) | (7 << 5) | (8 << 10) | (9 << 15) | (10 << 20) | (11 << 25);
-    ADC1->SQR3 = (0 << 0) | (1 << 5) | (2 << 10) | (3 << 15) | (4 << 20) | (5 << 25); // sample channel 10
-    /* ADC1->SQR1 = 0; */
-    /* ADC1->SQR2 = 0; */
-    /* ADC1->SQR3 = 0; */
-    ADC1->SMPR1 = 0xffffff;
-    ADC1->SMPR2 = 0x3fffffff;
-    /* ADC1->SMPR1 = (0b111 << 0); // sample time */
-
-    /* RCC->APB2RSTR &= ~(1 << 14); */
-    DMA->IFCR |= 0x0fffffff;  // clear all teh interruptz
-    static int16_t adcbuf[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    for (int i = 0; i < 10; ++i) {
-        adcbuf[i] = 0;
-    }
-    adcbuf[10] = 0xadde;
-
-    DMA_Channel1->CCR &= ~(1 << 0);
-    DMA_Channel1->CPAR = (size_t)&ADC1->DR;
-    DMA_Channel1->CMAR = (DMA_Channel1->CNDTR & 0xffff0000)  // keep reserved bits
-        | (size_t)&adcbuf[0]
-        ;
-    DMA_Channel1->CNDTR = 10;
-    DMA_Channel1->CCR = (DMA_Channel1->CCR & 0xffff8000)
-        | (0b11 << 12) // high prio
-        | (0b01 << 10) // 16 bit memory size
-        | (0b01 << 8) // 16 bit peripherial size
-        | (1 << 7) // memory inc
-        | (1 << 5) // circular mode
-        ;
-    DMA_Channel1->CCR |= (1 << 0);
-
-    ADC1->CR2 |= (1 << 0); // enable
-
-    DMA_Channel4->CCR &= ~(1 << 0); // channel disable
-    DMA_Channel4->CPAR = (size_t)&USART1->DR; // destination register
-    DMA_Channel4->CMAR = (size_t)&adcbuf[0]; // source buffer
-    DMA_Channel4->CNDTR =
-        (DMA_Channel4->CNDTR & 0xffff0000)  // keep reserved bits
-        | (sizeof(adcbuf)) // set the transfer size
-        ;
-    DMA_Channel4->CCR = (DMA_Channel4->CCR & 0xffff8000)
-        | (0b01 << 12) // medium prio
-        | (1 << 7) // memory inc
-        | (1 << 5) // circular mode
-        | (1 << 4) // read from memory
-        ;
-    USART1->SR &= ~(1 << 6); // clear Transmission Complete flag
     delay();
-    DMA_Channel4->CCR |=
-        (1 << 0) // channel enable
+
+    RCC->APB1RSTR = 0;
+    RCC->APB2RSTR = 0;
+
+    GPIOA->CRL =
+        GPIO_CRL_CNF0_0
+        | GPIO_CRL_CNF1_0
+        | GPIO_CRL_MODE2_1 | GPIO_CRL_CNF2_1
+        | GPIO_CRL_CNF3_0
+        | GPIO_CRL_CNF4_0
+        | GPIO_CRL_MODE5_1
+        | GPIO_CRL_CNF6_0
+        | GPIO_CRL_CNF7_0;
+    GPIOA->BSRR = GPIO_BSRR_BR5;
+
+    USART2->BRR = 312;
+    USART2->CR1 = USART_CR1_UE | USART_CR1_TE;
+
+    char buf[10];
+    buf[9] = 0;
+    buf[8] = '\n';
+
+    TIM2->CR1 = 0;
+    TIM2->CR2 = 0;
+    TIM2->SMCR =
+        TIM_SMCR_TS_1  // ITR2 (== TIM3)
+        | TIM_SMCR_MSM
+        | TIM_SMCR_SMS_0 | TIM_SMCR_SMS_1 | TIM_SMCR_SMS_2  // External Clock Mode 1
         ;
 
-    GPIOC->BSRR = 1<<8;
+    TIM3->CR1 = TIM_CR1_URS;
+    TIM3->CR2 = TIM_CR2_MMS_1;
+    TIM3->SR = 0;
+    TIM3->PSC = 23;
+    TIM3->DIER = 0;
+    TIM3->CCMR1 = TIM_CCMR1_CC1S_0 | TIM_CCMR1_CC2S_1;
+    TIM3->CCMR2 = 0;
+    TIM3->CCER = TIM_CCER_CC2P;
+    TIM3->SMCR = 0
+        | TIM_SMCR_MSM
+        | TIM_SMCR_TS_0 | TIM_SMCR_TS_2
+        | TIM_SMCR_SMS_2;
+    TIM3->CCER |= TIM_CCER_CC1E | TIM_CCER_CC2E;
+
+    TIM3->CR1 |= TIM_CR1_CEN;
+    TIM2->CR1 |= TIM_CR1_CEN;
+
+    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPDEN | RCC_APB2ENR_IOPEEN;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN | RCC_APB1ENR_USART2EN | RCC_APB1ENR_TIM2EN;
+
+    bool set = true;
     while (1) {
-        for (int i = 0; i < 10; ++i) {
-            if (i) {
-                GPIOC->BSRR = 1<<8;
-            } else {
-                GPIOC->BSRR = 1<<24;
-            }
-            /* adcbuf[0] = ADC1->DR; */
-            delay();
+        if (set) {
+            GPIOA->BSRR = GPIO_BSRR_BS5;
+        } else {
+            GPIOA->BSRR = GPIO_BSRR_BR5;
         }
+        set = !set;
+        delay();
+
+        uint32_to_hex(TIM3->CR1, buf);
+        puts("TIM3 CR1: ");
+        puts(buf);
+
+        uint32_to_hex(TIM3->SR, buf);
+        puts("TIM3 SR: ");
+        puts(buf);
+
+        /* const uint16_t ccr1 = TIM3->CCR1; */
+        /* const uint16_t ccr2 = TIM3->CCR2; */
+        /* uint32_to_hex(ccr1, buf); */
+        /* puts("TIM3 CCR1: "); */
+        /* puts(buf); */
+
+        /* uint32_to_hex(ccr2, buf); */
+        /* puts("TIM3 CCR2: "); */
+        /* puts(buf); */
+
+        uint32_to_hex(TIM2->CNT, buf);
+        puts("TIM2 CNT: ");
+        puts(buf);
+
+        uint32_to_hex(TIM3->CNT, buf);
+        puts("TIM3 CNT: ");
+        puts(buf);
+        /* USART2->DR = 'x'; */
     }
 
+    return 0;
+}
+
+
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+
+PUTCHAR_PROTOTYPE
+{
+    USART2->DR = ch;
+    while (!(USART2->SR & USART_FLAG_TC)) {
+
+    }
+    return ch;
+}
+
+int puts(const char *s) {
+    while (*s != 0) {
+        __io_putchar(*s);
+        s++;
+    }
     return 0;
 }
